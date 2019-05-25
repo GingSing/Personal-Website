c29a5a955ee820516a2509cac341f2776914efb0
Gary Yue <garyyue2017@gmail.com>
2019-05-24 18:12:00 -0700

    second

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `G.Y`,
+    title: `Gary Yue`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
